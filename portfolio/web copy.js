14c39c9dd4b6f017111340a929fb277d3d7d03a1
Utsavsingh45 <134831785+Utsavsingh45@users.noreply.github.com>
2023-06-05 23:58:23 +0530

    Add files via upload

---
+++ portfolio/web copy.js
@@ -0,0 +1,15 @@
+
+Swal.fire(
+  'WELCOME!',
+  'THIS IS MY PORTFOLIO',
+  'success'
+  
+)
+function geek() {
+  var g = prompt("What is your name!", "Name");
+  if (g != null) {
+  document.getElementById("g").innerHTML = "Welcome to " + a;
+  
+  }
+  }
+ 
